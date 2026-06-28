405e624bc2e7c6956c02d901b4a2cdd2a37c89d0
zrinafril7-png <zrinafril7@gmail.com>
2026-06-28 10:40:31 +0800

    Add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const tombol = document.querySelector("button");
+
+  if (tombol) {
+    tombol.addEventListener("click", () => {
+      alert("🌸 Selamat datang di Entertainment For You 💖");
+    });
+  }
+});
